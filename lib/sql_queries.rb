f4fbcf3ecb7b375a4a2d62e28f70fdee02282666
Matt Trussell <smtrussell@gmail.com>
2017-11-20 01:41:14 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,25 +7,66 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"Write your SQL query Here"
+"
+SELECT Projects.title, SUM(Pledges.amount)
+FROM Projects
+INNER JOIN Pledges
+ON Projects.id = Pledges.project_id
+GROUP BY Pledges.project_id
+ORDER BY Projects.title
+"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+"
+SELECT Users.name, Users.age, SUM(Pledges.amount)
+FROM Users
+JOIN Pledges
+ON Users.id = Pledges.user_id
+GROUP BY Pledges.user_id
+ORDER BY Users.name
+"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"
+SELECT Projects.title, SUM(Pledges.amount) - Projects.funding_goal
+FROM Projects
+INNER JOIN Pledges
+ON Projects.id = Pledges.project_id
+GROUP BY Pledges.project_id
+HAVING SUM(Pledges.amount) >= Projects.funding_goal
+ORDER BY Projects.title
+"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"
+SELECT Users.name, SUM(Pledges.amount)
+FROM Users
+INNER JOIN Pledges
+ON Users.id = Pledges.user_id
+GROUP BY Users.name
+ORDER BY SUM(Pledges.amount)
+"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"
+SELECT Projects.category, Pledges.amount
+FROM Projects
+INNER JOIN Pledges
+ON Projects.id = Pledges.project_id
+WHERE Projects.category = 'music'
+"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+"SELECT Projects.category, SUM(Pledges.amount)
+FROM Projects
+INNER JOIN Pledges
+ON Projects.id = Pledges.project_id
+WHERE Projects.category = 'books'
+GROUP BY Projects.category
+"
 end
